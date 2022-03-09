96055e8cf767460d645ae3e35f0922a672bfeb8b
YASHARORA6 <>
2022-03-09 17:13:00 +0000

    WS 1

--- Point.cpp
+++ Point.cpp
@@ -1,9 +1,43 @@
 #include "Point.h"
-
+#include <iostream>
+#include <math.h>
+using namespace std;
 // Notice how when we use field names in the functions that
 // belong to Point, we can just use the name without a dot operator.
 Point::Point() {
     x = 0.0;
     y = 0.0;
 }
+Point::Point(double xVal, double yVal) {
+  x = xVal;
+  y = yVal;
 
+}
+
+//double Point::getX() {
+  //print(x);
+  //}
+
+//double Point::getY() {
+   //print(y);
+  //}
+// Point = name of class
+  //getX = function definition
+//:: = whatever is after belongs to the class
+
+//void Point::setX(double xVal) {
+    //x = xVal;
+//}
+//void Point::setY(double yVal) {
+    //y = yVal;
+  //}
+
+void Point::print() {
+   cout << "x: " << x << " " << "y: " << y << endl;
+}
+double Point::calcDistance(Point other) {
+double xVal = pow(x - other.x, 2);
+double yVal = pow(y - other.y, 2);
+double dist = sqrt(xVal + yVal);
+return dist;
+}
--- Point.h
+++ Point.h
@@ -1,7 +1,15 @@
 class Point{
     public:
         Point();
+        //double getX();
+        //double getY();
+        //void setX(double xVal);
+        //void setY(double yVal);
+        void print();
+        Point(double xVal, double yVal);
+        double calcDistance(Point other);
     private:
         double x;
         double y;
 };
+
